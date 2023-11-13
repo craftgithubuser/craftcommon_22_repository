861b206c15ec153b57f1e08d9f2c712016304d3a
DerejeR <149451760+DerejeR@users.noreply.github.com>
2023-11-12 18:49:09 -0800

    commit from eclips

---
+++ samplejavaproject/src/samplejavaproject/com.java
@@ -0,0 +1,16 @@
+package samplejavaproject;
+
+public class com {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("printsmothing");
+System.out.println("printsmothing");
+System.out.println("printsmothing");
+System.out.println("printsmothing");
+System.out.println("printsmothing");
+System.out.println("printsmothing");
+System.out.println("printsmothing");
+}
+
+}
