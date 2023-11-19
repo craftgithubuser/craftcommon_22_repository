925e6143ed7173513f2302c579ec1a7a4157dfb7
Yibeltal Engida <149455987+yibetommy@users.noreply.github.com>
2023-11-18 21:11:10 -0500

    Add files via upload
    Yibeltal Engida

---
+++ SQL commands Quiz.sql
@@ -0,0 +1,21 @@
+create database MyDB;
+
+ USE MyDB;
+CREATE TABLE CUSTOMERS(
+customer_ID INT,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+age INT NOT NULL,
+gender CHAR NOT NULL,
+country CHAR NOT NULL);
+
+ALTER TABLE customers
+
+-- SELECT from customers
+ADD salary decimal(2, 5)
+
+-- drop table customer
+-- delete customer name;
+
+
+
